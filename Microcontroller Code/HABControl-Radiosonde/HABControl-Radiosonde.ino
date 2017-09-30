51aabfac670dd668e2e39de1ba1a1eb5ce0dc9b7
rdaruwala <rdaruwala@gmail.com>
2017-09-30 13:31:40 -0500

    Added a little intro documentation

--- Microcontroller Code/HABControl-Radiosonde/HABControl-Radiosonde.ino
+++ Microcontroller Code/HABControl-Radiosonde/HABControl-Radiosonde.ino
@@ -1,3 +1,26 @@
+/*The HABControl kit is supplied as is with no guarantees of performance or operation. Although the HABControl suite 
+ is designed for short-term Near Space operation, the conditions may exceed the specification of individual 
+ components which may fail to operate correctly because of low temperatures or pressures of increased 
+ radiation levels. We have no control over the effectiveness of your payload design especially the 
+ temperature and electromagnetic conditions  within, thus we make no warrnaty express or implied as to
+ the ability of our products to operate to any degree of effectiveness when used in your application.
+ 
+ If you decide to use this product under a balloon it’s your responsibility to ensure you comply with the 
+ local legislation and laws regarding meteorological balloon launching and radio transmission in the air.
+
+  The HABControl software and hardware is designed for hobbyist use and is not intended for mission-critical implementations.
+  We assume no responsibility or liability for any damages implicitly or explicitly caused by this product.
+ 
+ The Radiometrix MTX2 434Mhz is NOT license exempt in the United States of America and requires a radio
+ amateur license.
+ 
+
+ 
+ The latest code is available here : https://github.com/rdaruwala/HABControl
+*/
+
+
+
 #include <Wire.h>
 #include <SPI.h>
 #include "Adafruit_BME280.h"
