4b304fdd6cda1f9744f57a547e78c95a7f2091cb
rdaruwala <rdaruwala@gmail.com>
2017-09-30 13:21:40 -0500

    Added initial Microcontroller Code for the BME280

--- Microcontroller Code/HABControl-Radiosonde/HABControl-Radiosonde.ino
+++ Microcontroller Code/HABControl-Radiosonde/HABControl-Radiosonde.ino
@@ -1,3 +1,18 @@
+#include <Wire.h>
+#include <SPI.h>
+#include "Adafruit_BME280.h"
+#include "Adafruit_Sensor.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+
+Adafruit_BME280 bme; // I2C
+#define SEALEVELPRESSURE_HPA (1013.25) //CALIBRATION
+
+
 void setup() {
   // put your setup code here, to run once:
 
